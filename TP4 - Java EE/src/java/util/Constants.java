2c3e1e4c695bb98c70f18c86782148f5fcf3af46
elouen.dumas.pilhou <elouen.dumas.pilhou@gmail.com>
2018-12-06 11:39:32 +0100

    fix previous commit

--- TP4 - Java EE/src/java/util/Constants.java
+++ TP4 - Java EE/src/java/util/Constants.java
@@ -13,7 +13,7 @@ public class Constants {
 
     // DB
     public static final String DB_REQUEST_FROM_EMPLOYEES = "SELECT * FROM EMPLOYES";
-    public static final String DB_REQUEST_FROM_DB_USERS = "SELECT * FROM IDENTIFIANTS";
+    public static final String DB_REQUEST_FROM_DB_USERS = "SELECT * FROM UTILISATEURS";
     public static final String DB_REQUEST_REMOVE_EMPLOYEE = "DELETE FROM EMPLOYES WHERE ID = ?";
     public static final String DB_REQUEST_ADD_EMPLOYEE = "INSERT INTO EMPLOYES (NOM,PRENOM,TELDOM,TELPORT,TELPRO,ADRESSE,CODEPOSTAL,VILLE,EMAIL) VALUES ";
     public static final String DB_REQUEST_DELETE_EMPLOYEE = "DELETE FROM EMPLOYES WHERE ID =";
